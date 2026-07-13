734243b906c4a0a9dfca225c63788a671b278322
ambatuman <ditoriq@gmail.com>
2026-07-13 09:17:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ import io
 # Setup Page & Header
 st.set_page_config(page_title="Stok Tek Reconsil Tools", page_icon="⚙️", layout="wide")
 
-st.title("⚙️ Stok Tek Reconsil Tools")
-try:
-    st.image("halah_nyocot.jpg", width=200)
-except:
-   pass 
+st.title("⚙️ Stok Tek Reconsil Tools st.image("halah_nyocot.jpg", width=30)")
+    
 st.write("Aplikasi pintar untuk mencari pasangan barang yang selisih (otomatis menyembunyikan temuan yang tidak memiliki pasangan silang).")
 
 uploaded_file = st.file_uploader("Upload File Raw Excel Hasil Stock Take (.xlsx)", type=["xlsx"])
